fd19cd7dfa0b82b51ee2b20a494ddf54a4792e52
HAMMOUCHEFatiha <>
2021-04-07 22:17:30 +0000

    done

--- Principal.java
+++ Principal.java
@@ -1,17 +1,14 @@
 public class Principal
 {
-	public static void main(String[]args)
+public static void main(String[]args)
   {
-	
-		if ( args.length == 1 )
-		{
-			String motAverifier = args[0] ; // args[0] correspond a l'argument 2 de la commande java
-		
-			boolean isPalindrome = Palindrome.is_palindrome(motAverifier) ;
-		
-			System.out.println("----------------------------------------------------");
-			System.out.println("Input : " + motAverifier + " >>> Output : " + isPalindrome );		
-			System.out.println("----------------------------------------------------");		
-		}
+if ( args.length == 1 )
+{
+String s = args[0] ; // args[0] correspond a l'argument 2 de la commande java
+boolean isPalindrome = Palindrome.is_palindrome(s) ;
+System.out.println("----------------------------------------------------");
+System.out.println("Input : " + s + " >>> Output : " + isPalindrome );
+System.out.println("----------------------------------------------------");
+}
   }
 }
